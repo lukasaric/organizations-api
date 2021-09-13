b77e6db3169eb08792c1d235fcae98a525fb93d0
Luka Saric <rpg.ari7@gmail.com>
2021-09-13 15:58:12 +0200

    Added db scripts & migration fix

--- src/shared/database/migrations/Migration20210913090241.ts
+++
@@ -1,8 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20210913090241 extends Migration {
-  async up(): Promise<void> {
-    this.addSql('create table "user" ("id" serial primary key, "first_name" varchar(255) not null, "last_name" varchar(255) not null, "email" varchar(255) not null, "role" text check ("role" in (\'\')) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null);');
-    this.addSql('alter table "user" add constraint "user_email_unique" unique ("email");');
-  }
-}
---
+++ src/shared/database/migrations/Migration20210913135216.ts
@@ -0,0 +1,27 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20210913135216 extends Migration {
+  async up(): Promise<void> {
+    const knex = this.getKnex();
+    const ROLES = ['admin', 'user'];
+    const createUserTable = knex.schema.createTable('user', table => {
+      table.increments('id');
+      table.enum('role', ROLES);
+      table.string('first_name').notNullable();
+      table.string('last_name').notNullable();
+      table.string('email').notNullable().unique();
+      table.timestamp('created_at', { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now());
+      table.timestamp('updated_at', { useTz: true })
+        .notNullable()
+        .defaultTo(knex.fn.now());
+    });
+    this.addSql(createUserTable.toQuery());
+  }
+
+  async down(): Promise<void> {
+    const knex = this.getKnex();
+    this.addSql(knex.schema.dropTable('user').toQuery());
+  }
+}
