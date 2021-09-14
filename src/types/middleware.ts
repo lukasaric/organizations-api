8e2b12fbf7e6861b8742e3316f5d7d14587c9b30
Luka Saric <rpg.ari7@gmail.com>
2021-09-14 19:51:05 +0200

    Changed mw interfaces return types

--- src/types/middleware.ts
+++ src/types/middleware.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpError } from 'http-errors';
 
 export interface IErrorMiddleware {
-  handle(err: HttpError | Error, req: Request, res: Response, next: NextFunction): void;
+  handle(err: HttpError | Error, req: Request, res: Response, next: NextFunction): Response | void;
 }
 
 export interface IMiddleware {
-  handle(req: Request, res: Response, next: NextFunction, id?: string): void;
+  handle(req: Request, res: Response, next: NextFunction, id?: string): Response | void;
 }
